1f918c752bc6c26eb65d109fdc0d20a63e042df2
lxq <1628513134@qq.com>
2021-12-04 17:02:35 +0800

    hide django secret_key

--- my_blog/settings/common.py
+++ my_blog/settings/common.py
@@ -13,19 +13,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+(yri0xvw3svd5*#zt28mncvs*t=jphhh@sjjf5@17v1hf0pnm'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.lxq.icu']
 
 
 # Application definition
